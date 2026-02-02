39b8a428278373b83075c26d3e0a591a7841a816
IlyaSigma111 <ilyagulkov25@gmail.com>
2026-02-02 21:52:59 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,15 +1,32 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
-import { getDatabase, ref, set, get, update, onValue, push, remove, onChildAdded, onChildChanged } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { 
+    getAuth, 
+    GoogleAuthProvider, 
+    signInWithPopup, 
+    signOut, 
+    onAuthStateChanged 
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+import { 
+    getDatabase, 
+    ref, 
+    set, 
+    get, 
+    update, 
+    onValue, 
+    push, 
+    remove, 
+    onChildAdded, 
+    onChildChanged 
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAoZollSKuSUr3GG4eYOdz5AxgUGf304HI",
-  authDomain: "neocascadecoop.firebaseapp.com",
-  databaseURL: "https://neocascadecoop-default-rtdb.firebaseio.com",
-  projectId: "neocascadecoop",
-  storageBucket: "neocascadecoop.firebasestorage.app",
-  messagingSenderId: "624760450085",
-  appId: "1:624760450085:web:1db92536b7ab4d1b5963cc"
+    apiKey: "AIzaSyAoZollSKuSUr3GG4eYOdz5AxgUGf304HI",
+    authDomain: "neocascadecoop.firebaseapp.com",
+    databaseURL: "https://neocascadecoop-default-rtdb.firebaseio.com",
+    projectId: "neocascadecoop",
+    storageBucket: "neocascadecoop.firebasestorage.app",
+    messagingSenderId: "624760450085",
+    appId: "1:624760450085:web:1db92536b7ab4d1b5963cc"
 };
 
 // Инициализация Firebase
@@ -18,4 +35,24 @@ const auth = getAuth(app);
 const database = getDatabase(app);
 const provider = new GoogleAuthProvider();
 
-export { auth, database, provider, signInWithPopup, signOut, onAuthStateChanged, ref, set, get, update, onValue, push, remove, onChildAdded, onChildChanged };
+// Добавляем scope для получения email
+provider.addScope('email');
+provider.addScope('profile');
+
+export { 
+    auth, 
+    database, 
+    provider, 
+    signInWithPopup, 
+    signOut, 
+    onAuthStateChanged,
+    ref, 
+    set, 
+    get, 
+    update, 
+    onValue, 
+    push, 
+    remove, 
+    onChildAdded, 
+    onChildChanged 
+};
